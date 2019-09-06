47ae5eaa94ca5a54ae53fc9bb6c158ca090067a9
snake <18501604724@163.com>
2019-09-06 18:00:27 +0800

    update AssetBundle

---
+++ Assets/Scripts/AssetBundle/AssetBundleGeScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleGeScript
+{
+
+    public void UpLoadAssetBundle(string name){
+       //
+       
+    }
+}
---
+++ Assets/Scripts/AssetBundle/AssetBundleSerialization.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+
+
+
+
+[CreateAssetMenu(menuName="Create ScriptableObject ")]
+public class AssetBundleSerialization : ScriptableObject
+{
+    [System.Serializable]
+    public struct AssetBundleInfo
+    {
+        public string FileName;
+        public bool isChange;
+    }
+    public List<AssetBundleInfo>  abInfos = new List<AssetBundleInfo>();
+    private static AssetBundleSerialization _instance;
+    public static AssetBundleSerialization Instance
+    {
+        get
+        {
+            if (!_instance)
+                _instance = Resources.FindObjectsOfTypeAll<AssetBundleSerialization>().FirstOrDefault();
+#if UNITY_EDITOR
+            if (!_instance)
+            {
+                var abSerial = CreateInstance<AssetBundleSerialization>();   
+                UnityEditor.AssetDatabase.CreateAsset(abSerial,"Assets/AssetBundle.asset");
+            }
+            else
+            {
+                UnityEditor.AssetDatabase.LoadAssetAtPath<AssetBundleSerialization>("Assets/AssetBundle.asset");
+            }
+#endif
+            return _instance;
+        }
+    }
+
+}
